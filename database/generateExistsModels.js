01dda013ca1de79867aa69e403fcefa830951b74
titusefferian <kiritosato@gmail.com>
2019-01-24 00:31:49 +0700

    rl_user_model

--- database/generateExistsModels.js
+++ database/generateExistsModels.js
@@ -3,7 +3,10 @@ var auto = new SequelizeAuto('ralaliweb_db','intern_Rll_2018','Ralali2018_RLL_In
     host:'149.129.214.194',
     port:3308,
     dialect:'mysql',
-    tables:['rl_user_key']
+    tables:['rl_user_key'],
+    additional: {
+        timestamps: false
+    },
 })
 
 auto.run(function(err){
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
+import express from 'express'
+var models = require('../database/models')
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+  models.rl_user_key.findAll().then(function (result) {
+    res.status(200).send(result)
+  })
 });
 
 export default router
