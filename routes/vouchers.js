e3ef576b3629aa1aff387d1811e94a23587768ac
titusefferian <kiritosato@gmail.com>
2019-01-24 09:42:34 +0700

    add list voucher routes

--- routes/vouchers.js
+++ routes/vouchers.js
@@ -1,11 +1,17 @@
 import express from 'express'
 var router = express.Router();
-
-router.get('/', function (req, res) {
-  res
-    .status(200)
-    .send({
-      data:'welcome to voucher routes'
+import models from '../database/models'
+router.get('/list', function (req, res) {
+    /**
+     * butuh diganti menggunakan models,
+     * yang ini masih menggunakan query mentah
+     */
+    models.sequelize.query('SELECT * FROM rl_redeem_points as rp JOIN rl_ms_promo as mp ON rp.promo_id = mp.id WHERE mp.number_of_ticket_promo > 0')
+    .then(function(result){
+        res.status(200).send(result[0])
+    })
+    .catch(function(error){
+        res.status(500).send(error)
     })
 });
 
